b14b0bc90622a596a32f969f3548eb9f3045705f
Alexandros Gaitanis <alexandros.gaitanis@gmail.com>
2020-11-30 22:06:55 +0200

    Preprocessing - Stored images in numpy arrays

--- project_1/mnist.py
+++ project_1/mnist.py
@@ -1,1 +1,28 @@
+import scipy.io
+import numpy as np
+
+
+mat = scipy.io.loadmat('data/mnist_all.mat')
+
+x_train = []
+y_train = []
+x_test = []
+y_test = []
+
+for key, data in mat.items():
+    if 'train' in key:
+        for x in data:
+            x_train.append(x)
+            num = int(key[-1])
+            y_train.append(num % 2)
+    if 'test' in key:
+        for x in data:
+            x_test.append(x)
+            num = int(key[-1])
+            y_test.append(num % 2)
+
+x_train = np.array(x_train)
+y_train = np.array(y_train)
+x_test = np.array(x_test)
+y_test = np.array(y_test)
 
