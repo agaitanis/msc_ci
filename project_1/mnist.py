1fc31986e225affc72435cfb2c094fbf6a253af8
Alexandros Gaitanis <alexandros.gaitanis@gmail.com>
2020-12-01 21:39:35 +0200

    Refactoring

--- project_1/mnist.py
+++ project_1/mnist.py
@@ -1,53 +1,46 @@
 import scipy.io
 import numpy as np
-from sklearn import svm, metrics, utils, decomposition
+import sklearn
 import time
 
-mat = scipy.io.loadmat('data/mnist_all.mat')
-
-x_train = []
-y_train = []
-x_test = []
-y_test = []
-
-for key, data in mat.items():
-    if 'train' in key:
-        for x in data:
-            x_train.append(x)
-            num = int(key[-1])
-            y_train.append(num % 2)
-    if 'test' in key:
-        for x in data:
-            x_test.append(x)
-            num = int(key[-1])
-            y_test.append(num % 2)
-
-x_train = np.array(x_train)
-y_train = np.array(y_train)
-x_test = np.array(x_test)
-y_test = np.array(y_test)
-
-x_train, y_train = utils.shuffle(x_train, y_train, random_state=0)
-
-x_train = x_train/255.
-y_train = y_train
-x_test = x_test/255.
-y_test = y_test
+def load_mnist():
+    mat = scipy.io.loadmat('data/mnist_all.mat')
+    x_train, y_train, x_test, y_test = [], [], [], []
+    
+    for key, data in mat.items():
+        if 'train' in key:
+            for x in data:
+                x_train.append(x)
+                num = int(key[-1])
+                y_train.append(num % 2)
+        if 'test' in key:
+            for x in data:
+                x_test.append(x)
+                num = int(key[-1])
+                y_test.append(num % 2)
+                
+    return np.array(x_train), np.array(y_train), np.array(x_test), np.array(y_test)
 
 
-pca = decomposition.PCA(0.9)
-x_train = pca.fit_transform(x_train)
-print('x_train.shape =', x_train.shape)
+x_train, y_train, x_test, y_test = load_mnist()
 
-svm = svm.SVC(C=1.0)
+scaler = sklearn.preprocessing.MinMaxScaler()
+pca = sklearn.decomposition.PCA(0.9)
+svm = sklearn.svm.SVC(C=1.0)
+
+pipe = sklearn.pipeline.Pipeline([
+    ('scaler', scaler),
+    ('pca', pca),
+    ('svm', svm)
+    ])
 
 t1 = time.time()
-svm.fit(x_train, y_train)
+pipe.fit(x_train, y_train)
 t2 = time.time()
+
 print("Training time: {:.1f} min".format((t2 - t1)/60))
 
-x_test = pca.transform(x_test)
-y_pred = svm.predict(x_test)
-print('Accuracy =', metrics.accuracy_score(y_test, y_pred))
+y_pred = pipe.predict(x_test)
+print('Accuracy =', sklearn.metrics.accuracy_score(y_test, y_pred))
 
 
