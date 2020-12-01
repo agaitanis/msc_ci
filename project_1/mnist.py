0b0bdec2be413a5476156f0b48d1eb20eba7b904
agaitanis <alexandros.gaitanis@gmail.com>
2020-12-01 18:00:11 +0200

    First attempt

--- project_1/mnist.py
+++ project_1/mnist.py
@@ -1,6 +1,7 @@
 import scipy.io
 import numpy as np
-
+from sklearn import svm, metrics, utils, decomposition
+import time
 
 mat = scipy.io.loadmat('data/mnist_all.mat')
 
@@ -25,4 +26,28 @@ x_train = np.array(x_train)
 y_train = np.array(y_train)
 x_test = np.array(x_test)
 y_test = np.array(y_test)
+
+x_train, y_train = utils.shuffle(x_train, y_train, random_state=0)
+
+x_train = x_train/255.
+y_train = y_train
+x_test = x_test/255.
+y_test = y_test
+
+
+pca = decomposition.PCA(0.9)
+x_train = pca.fit_transform(x_train)
+print('x_train.shape =', x_train.shape)
+
+svm = svm.SVC(C=1.0)
+
+t1 = time.time()
+svm.fit(x_train, y_train)
+t2 = time.time()
+print("Training time: {:.1f} min".format((t2 - t1)/60))
+
+x_test = pca.transform(x_test)
+y_pred = svm.predict(x_test)
+print('Accuracy =', metrics.accuracy_score(y_test, y_pred))
+
 
